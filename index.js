66f6bfedcb42927370be3b4cba8091f4e67da57d
ByWaleed <waleedi825@gmail.com>
2022-01-31 11:20:02 +0000

    home, seed and random routes implemented and connected with DB

--- index.js
+++ index.js
@@ -5,3 +5,48 @@ import { open } from 'sqlite'
 import fetch from 'node-fetch'
 
 const app = express();
+
+// Views Setup
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+app.use(express.static('public'))
+
+const dbPromise = open({
+    filename: 'data.db',
+    driver: sqlite3.Database
+})
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/seed', async (req, res) => {
+    const db = await dbPromise
+    fetch('https://raw.githubusercontent.com/15Dkatz/official_joke_api/master/jokes/index.json')
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach(async (joke) => {
+                await db.run('INSERT INTO Jokes (category, setup, punchline) VALUES (?, ?, ?);', joke.type, joke.setup, joke.punchline)
+            });
+        })
+    res.redirect('/')
+})
+
+app.get('/random', async (req, res) => {
+    const db = await dbPromise
+    const joke = await db.all('SELECT * FROM Jokes ORDER BY RANDOM() LIMIT 1;')
+    res.json(joke[0])
+})
+
+// App Setup
+const setup = async () => {
+    const db = await dbPromise
+    await db.migrate()
+    app.listen(8000, () => {
+        console.log('listening on localhost:8000')
+    })
+}
+
+setup()
